b7a88d41cae7bb004ae32f880ee381a1c56c55ef
pedromolina17 <pedro.molinanoa@gmail.com>
2024-09-13 07:21:16 -0500

    Add Auth

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -2,14 +2,30 @@
 import { useForm } from "react-hook-form";
 import { login } from "../services/auth";
 import Cookie from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+
 const Login = () => {
   const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
   const onSubmit = handleSubmit(async (data) => {
     try {
       const res = await login(data);
       const { access_token } = res;
+
       if (access_token) {
         Cookie.set("token", access_token, { expires: 7 });
+
+        const decodedToken = jwtDecode(access_token);
+
+        if (decodedToken.role === 1) {
+          router.push("/admin");
+        } else if (decodedToken.role === 2) {
+          router.push("/user");
+        } else {
+          console.error("Role no reconocido");
+        }
       } else {
         console.error("No access token found in response");
       }
@@ -17,11 +33,16 @@ const Login = () => {
       console.log(error);
     }
   });
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" {...register("email")} />
-        <input type="text" {...register("password")} />
+        <input type="text" {...register("email")} placeholder="Email" />
+        <input
+          type="password"
+          {...register("password")}
+          placeholder="Password"
+        />
         <button type="submit">Login</button>
       </form>
     </div>
